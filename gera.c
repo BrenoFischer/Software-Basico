0b50389c722e603ea7f8f82b163d65ec21634004
BrenoFischer <42100477+BrenoFischer@users.noreply.github.com>
2019-06-19 10:16:43 -0300

    Add files via upload

--- gera.c
+++ gera.c
@@ -7,6 +7,7 @@ static void error (const char *msg, int line) {
   exit(EXIT_FAILURE);
 }
 
+/* funcoes auxiliares */
 int atribuiMovVariavel (int idx) {
   if (idx == 1)
     return 0xfc;
@@ -29,6 +30,53 @@ int atribuiMovParam (int idx) {
     return 0x55;
 }
 
+void movVar1Const (unsigned char *codigo, int i, int idx) {
+  /* mov const1 para aux1 */
+  unsigned char byte;
+  int j;
+  codigo[i] = 0xb9;
+  i++;
+  for (j=0;j<4;j++) {
+    byte = (idx >> (8*j)) & 0xFF;
+    codigo[i+j] = byte;
+   }
+   i+=4;
+   /* fim mov const1 para aux1 */
+}
+void movVar2Const (unsigned char *codigo, int i, int idx) {
+    /* mov const2 para aux2 */
+    unsigned char byte;
+    int j;
+    codigo[i] = 0x41;
+    codigo[i+1] = 0xb8;
+    i+=2;
+    for (j=0;j<4;j++) {
+      byte = (idx >> (8*j)) & 0xFF;
+      codigo[i+j] = byte;
+    }
+   /* fim mov const2 para aux2 */
+}
+void movVar1V (unsigned char *codigo, int i, int idx) {
+  /* mov var 1 para aux1 */
+  codigo[i] = 0x8b;
+  codigo[i+1] = 0x4d;
+  codigo[i+2] = atribuiMovVariavel(idx);
+  /* fim mov var1 para aux1 */
+}
+
+void movVar2V (unsigned char *codigo, int i, int idx) {
+  codigo[i] = 0x44;
+  codigo[i+1] = 0x8b;
+  codigo[i+2] = 0x45;
+  codigo[i+3] = atribuiMovVariavel(idx);
+}
+
+void movFimOperacaoA (unsigned char *codigo, int i, int idx) {
+  codigo[i] = 0x89;
+  codigo[i+1] = 0x4d;
+  codigo[i+2] = atribuiMovVariavel(idx);
+}
+/* ****** fim funcoes auxiliares ******* */
 
 
 funcp gera(FILE *f) {
@@ -36,7 +84,7 @@ funcp gera(FILE *f) {
   int i,c,j=0,line=1;;
 
   /* aloca um valor equialente a 4000 bytes(abre espaco para o pior caso para 30 linhas de comandos    Simples) */
-  codigo = (unsigned char *) malloc (30*sizeof(char));
+  codigo = (unsigned char *) malloc (4000*sizeof(char));
 
   codigo[0] = 0x55; /* push %rbp */
   /* mov %rsp, %rbp */
@@ -44,7 +92,7 @@ funcp gera(FILE *f) {
   codigo[2] = 0x89; 
   codigo[3] = 0xe5;
   /* ************* */
-  /* sub 0x10, %rsp */
+  /* sub 0x16, %rsp */
   codigo[4] = 0x48;
   codigo[5] = 0x83;
   codigo[6] = 0xec;
@@ -77,17 +125,12 @@ funcp gera(FILE *f) {
           codigo[i+2] = atribuiMovVariavel(idx0);
           i+=3;
         }
-        else { //retorna parametro
-          codigo[i] = 0x89;
-          //inserir o outro byte (descobrir os valores de edi, esi e edx)
-          i+=2;
-        }
         break;        
       }
       
       case 'v': { /* atribuicao e operacao aritmetica */
         int idx0, idx1;
-        char var0 = c, c0, var1;
+        char c0, var1;
         if (fscanf(f, "%d %c", &idx0, &c0) != 2)
           error("comando invalido", line);
 
@@ -107,22 +150,106 @@ funcp gera(FILE *f) {
           }
           else if (var1 == 'p') { //atribui parametro
             codigo[i] = 0x89;
-            i++;
-            codigo[i] = atribuiMovParam(idx1);
-            i++;
-            codigo[i] = atribuiMovVariavel(idx0);
-            i++;
+            codigo[i+1] = atribuiMovParam(idx1);
+            codigo[i+2] = atribuiMovVariavel(idx0);
+            i+=3;
+          }
+          else { //atribui variavel
+            /* passa para uma aux o valor da variavel a ser atribuida */
+            codigo[i] = 0x8b;
+            codigo[i+1] = 0x4d;
+            codigo[i+2] = atribuiMovVariavel(idx1);
+            /* ********************** */
+            /* passa a aux para a variavel a atribuir */
+            codigo[i+3] = 0x89;
+            codigo[i+4] = 0x4d;
+            codigo[i+5] = atribuiMovVariavel(idx0);
+            /* ********************** */
+            i+=6;
           }
         }
-        else { /* operacao aritmetica */
+        else { //operacao aritmetica
           char var2, op;
           int idx2;
           if (c0 != '=')
+            error ("comando invalido", line);
+          if (fscanf(f, " %c%d %c %c%d", &var1, &idx1, &op, &var2, &idx2) != 5)
             error("comando invalido", line);
-          if (fscanf(myfp, " %c%d %c %c%d", &var1, &idx1, &op, &var2, &idx2) != 5)
-            error("comando invalido", line);
-          printf("%d %c%d = %c%d %c %c%d\n", 
-                 line, var0, idx0, var1, idx1, op, var2, idx2);
+
+          if (op == '-') { //operacao = '-'
+            if (var1 == '$') { //var 1 = constante
+              movVar1Const(codigo, i, idx1);
+              i+=5;
+
+              if (var2 == '$') { //CONST - CONST
+                movVar2Const(codigo, i, idx2);
+                i+=6;
+              }
+
+              else if (var2 == 'v') { //CONST - V
+                movVar2V(codigo, i, idx2);
+                i+=4;
+              } 
+            }
+            else if (var1 == 'v') { //var1 = v
+              movVar1V(codigo, i, idx1);
+              i+=3;
+
+              if (var2 == 'v') { //V - V
+                movVar2V(codigo, i, idx2);
+                i+=4;
+              }
+              else if (var2 == '$') { //V - CONST
+                movVar2Const(codigo, i, idx2);
+                i+=6;
+              }
+            }
+            /* sub aux2=aux1 - aux2 */
+            codigo[i] = 0x44;
+            codigo[i+1] = 0x29;
+            codigo[i+2] = 0xc1;
+            i+=3;
+            /* fim sub ************* */
+          }
+          else if (op == '+') { //operacao '+'
+            if (var1 == 'v') { //var1 = v
+              movVar1V(codigo, i, idx1);
+              i+=3;
+              if (var2 == 'v') { // V + V
+                movVar2V(codigo, i, idx2);
+                i+=4;
+              }
+              else if (var2 == '$'){ //V + CONST
+                movVar2Const(codigo, i, idx2);
+                i+=6;
+              }
+            }
+            else if (var1 == '$') { //var 1 = const
+              movVar1Const(codigo, i, idx1);
+              i+=5;
+              if (var2 == '$') { //Const + const
+                movVar2Const(codigo, i, idx2);
+                i+=6;
+              }
+              else if (var2 == 'v') { //const + v
+                movVar2V(codigo, i, idx2);
+                i+=4;
+              }
+            }
+            /* add aux2=aux1 + aux2 */
+            codigo[i] = 0x44;
+            codigo[i+1] = 0x01;
+            codigo[i+2] = 0xc1;
+            i+=3;
+            /* fim add ************ */
+          }
+          else if (op == '*') { //operacao '*'
+
+          }
+          /* ****** mov aux2 para v ******* */
+          movFimOperacaoA(codigo, i, idx0);
+          i+=3;
+          /* ******* fim mov ********* */ 
         }
         break;
       }
